b42b58d414293e5edc3d8e204c798404d8408b53
chengxinyong <chengxinyong@139.com>
2019-10-13 23:16:19 +0800

    eventHub缓存调用队列，可延迟执行

--- event-hub.js
+++ event-hub.js
@@ -1,12 +1,21 @@
 window.eventHub = {
     events: {},
+    queue: {},
     emit(eventName, data) {
         let fnList = this.events[eventName]
         if (fnList) {
             fnList.map(fn => fn.call(undefined, data))
+        } else {
+            this.queue[eventName] = [...(this.queue[eventName] || []), data]
         }
     },
     on(eventName, fn) {
         (this.events[eventName] || (this.events[eventName] = [])).push(fn)
+
+        let remainDataArray = this.queue[eventName]
+        if (remainDataArray) {
+            remainDataArray.forEach(data => this.emit(eventName, data))
+            delete this.queue[eventName]
+        }
     }
 }
