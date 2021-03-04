79159a89cb6a88cd823051ab3b225beca8af51c8
chengxinyong <chengxinyong@139.com>
2021-03-04 09:02:51 +0800

    时间格式化

--- function.js
+++ function.js
@@ -49,6 +49,29 @@ window.$ = (function (window, $) {
     const toCamelCase = (name) => name.toLowerCase().replace(/_(\w)/g, (x) => {return x[1].toUpperCase()})
 
     /**
+     * 时间格式化
+     */
+    const dateFormat = (fmt, date) => {
+        let ret;
+        const opt = {
+            "Y+": date.getFullYear().toString(),        // 年
+            "m+": (date.getMonth() + 1).toString(),     // 月
+            "d+": date.getDate().toString(),            // 日
+            "H+": date.getHours().toString(),           // 时
+            "M+": date.getMinutes().toString(),         // 分
+            "S+": date.getSeconds().toString()          // 秒
+            // 有其他格式化字符需求可以继续添加，必须转化成字符串
+        };
+        for (let k in opt) {
+            ret = new RegExp("(" + k + ")").exec(fmt);
+            if (ret) {
+                fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+            };
+        };
+        return fmt;
+    }
+
+    /**
      * 复制文本
      */
     const copy = function (text) {
@@ -232,6 +255,7 @@ window.$ = (function (window, $) {
         bindEventForce: bindEventForce,
         firstUpperCase: firstUpperCase,
         toCamelCase: toCamelCase,
+        dateFormat: dateFormat,
         copy: copy,
         log: log,
         errorMsg: errorMsg,
